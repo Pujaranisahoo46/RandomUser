137c57730dc81bf075dd85237b377b45dc6feb9e
Pujaranisahoo46 <abhipshasahoo46@gmail.com>
2024-08-06 00:22:15 +0530

    Random user

--- script.js
+++ script.js
@@ -1,28 +1,101 @@
-// addEventListener("click", function(){})
+var randomname,randomlocation,randomemail,randompassword,randomdob,randomphone;
+var image=document.getElementById('user-image');
+var iconregular=document.querySelectorAll('.fa-regular');
+var iconsolid=document.querySelectorAll('.fa-solid');
+var username=document.getElementById('user-name');
+var userdesc=document.getElementById('user-desc');
 
+document.addEventListener("DOMContentLoaded", generateUser);
+function generateUser(){
+    fetch('https://randomuser.me/api/')
+    .then((response) => response.json())
+    .then((data)=>{
+        const user =data.results[0];
+        image.src=`${user.picture.large}`;
+        randomname=`${user.name.first} ${user.name.last}`;
+        randomemail=user.email;
+        randomdob=user.dob.date;
+        randomphone=user.phone;
+        randomlocation=`${user.location.street.number} ${user.location.street.name}`;
+        randompassword=user.login.password;
+        username.innerText="Hi, My name is";
+        userdesc.innerText=randomname;
+        iconregular[0].style.color="green"
 
-document.addEventListener('DOMContentLoaded', () => {
-    const usersContainer = document.getElementById('users');
-
-    // Function to create user elements
-    function createUserElement(user) {
-        const userElement = document.createElement('div');
-        userElement.classList.add('user');
-        userElement.innerHTML = `
-            <h2>${user.name}</h2>
-            <p>Email: ${user.email}</p>
-        `;
-        return userElement;
+    })
+    .catch((error)=>{
+        console.error("Error fetching user:",error);
+    });
     }
-
-    // Fetch users from API
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => {
-            users.forEach( user=> {
-                const userElement = createUserElement(user);
-                usersContainer.appendChild(userElement);
-            });
-        })
-        .catch(error => console.error('Error fetching users:', error));
-});
+    iconregular[0].addEventListener('mouseover',generatename)
+    function generatename(){
+        username.innerText="Hi, My name is";
+        userdesc.innerText=randomname;
+        iconregular[0].style.color="green"
+        iconregular[1].style.color="gray"
+        iconregular[2].style.color="gray"
+    }
+    iconregular[1].addEventListener('mouseover',generatemail)
+    function generatemail(){
+        username.innerText="My email address is";
+        userdesc.innerText=randomemail;
+        iconregular[1].style.color="green"
+        iconregular[0].style.color="gray"
+        iconregular[2].style.color="gray"
+    }
+    iconregular[2].addEventListener('mouseover',generatedob)
+    function generatedob(){
+        username.innerText="My birthday is";
+        userdesc.innerText=randomdob;
+        iconregular[2].style.color="green"
+        iconregular[0].style.color="gray"
+        iconregular[1].style.color="gray"
+        iconsolid[0].style.color="gray"
+        iconsolid[1].style.color="gray"
+        iconsolid[2].style.color="gray"
+    }
+    iconregular[2].addEventListener('mouseover',generatedob)
+    function generatedob(){
+        username.innerText="My birthday is";
+        userdesc.innerText=randomdob;
+        iconregular[2].style.color="green"
+        iconregular[0].style.color="gray"
+        iconregular[1].style.color="gray"
+        iconsolid[0].style.color="gray"
+        iconsolid[1].style.color="gray"
+        iconsolid[2].style.color="gray"
+    }
+    iconsolid[0].addEventListener('mouseover',generatlocation)
+    function generatlocation(){
+        username.innerText="My address is";
+        userdesc.innerText=randomlocation;
+        iconregular[0].style.color="gray"
+        iconregular[1].style.color="gray"
+        iconregular[2].style.color="gray"
+        iconsolid[0].style.color="green"
+        iconsolid[1].style.color="gray"
+        iconsolid[2].style.color="gray"
+    }
+    iconsolid[1].addEventListener('mouseover',generatephone)
+    function generatephone(){
+        username.innerText="My phone number is";
+        userdesc.innerText=randomphone;
+        iconregular[0].style.color="gray"
+        iconregular[1].style.color="gray"
+        iconregular[2].style.color="gray"
+        iconsolid[0].style.color="gray"
+        iconsolid[1].style.color="green"
+        iconsolid[2].style.color="gray"
+    }
+    iconsolid[2].addEventListener('mouseover',generatepassword)
+    function generatepassword(){
+        username.innerText="My password is";
+        userdesc.innerText=randompassword;
+        iconregular[0].style.color="gray"
+        iconregular[1].style.color="gray"
+        iconregular[2].style.color="gray"
+        iconsolid[0].style.color="gray"
+        iconsolid[1].style.color="gray"
+        iconsolid[2].style.color="green"
+    }
+   
